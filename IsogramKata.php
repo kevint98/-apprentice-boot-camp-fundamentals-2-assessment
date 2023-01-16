ac029a5247b91613c307db7e99dce41505049098
Kevin-aps <kevin.thompson@thinkaps.com>
2023-01-16 11:38:20 +0000

    refactor code for readability

--- IsogramKata.php
+++ IsogramKata.php
@@ -4,10 +4,16 @@ namespace Fundamentals2\IsogramKata;
 
 class IsogramKata
 {
+    // public function isIsogram($word): bool
+    // {
+    //     $wordAsArray = str_split(strtolower($word));
+    //     $uniqueArray = array_unique($wordAsArray);
+    //     return $wordAsArray == $uniqueArray;
+    // }
+
     public function isIsogram($word): bool
     {
-        $wordAsArray = str_split(strtolower($word));
-        $uniqueArray = array_unique($wordAsArray);
-        return $wordAsArray == $uniqueArray;
+        $array = str_split(strtolower($word));
+        return count($array) === count(array_unique($array));
     }
 }
